212065b3a2246bb6dc62ab573db0e547212158a8
Thomas Masters <thomasmasters@gmail.com>
2020-06-24 06:56:14 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Thomas Masters on 6/24/20.
+//  Copyright © 2020 Thomas Masters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
